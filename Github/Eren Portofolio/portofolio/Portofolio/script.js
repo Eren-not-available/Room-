075da54701428deec91f9073add795b4e6c401dd
Eren-not-available <76025009+Eren-not-available@users.noreply.github.com>
2021-02-09 17:50:02 +0100

    Delete Github directory

--- Github/Eren Portofolio/portofolio/Portofolio/script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const bava = document.getElementsByClassName("bava")[0];
-
-toggleButton.addEventListener("click", () => {
-  bava.classList.toggle("active");
-});
